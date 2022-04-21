b9d23f0e4a3437feaf0f48d901711f671f39effb
shakeebanwar <47249086+shakeebanwar@users.noreply.github.com>
2022-04-21 10:01:22 +0500

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
+from channels.layers import get_channel_layer
+from chat.models import *
 
+channel_layer = get_channel_layer()
 # Create your views here.
 
 
@@ -12,3 +16,30 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+
+from asgiref.sync import sync_to_async
+
+@sync_to_async
+def dbQueries():
+
+    data = users.objects.all().values('id','name')
+    finalarray = list()
+    for j in data:
+        finalarray.append({'id':j['id'],'name':j['name']})
+    
+    return finalarray
+
+
+
+async def trigger(request):
+   
+    data = await dbQueries()
+
+    await channel_layer.group_send("notification_hey", {
+        "type":"notification_message",
+        "message":"hello safdar",
+        "data":data
+
+    })
+    return JsonResponse({'status':True,'message':"trigger"})
